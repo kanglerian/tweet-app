0b4cf93fbea329cc647ded06d703da0bb0b1fa4f
kanglerian <kanglerian@gmail.com>
2021-09-14 15:25:31 +0700

    Ruby on Rails5 III

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,13 @@
 class PostController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
+  end
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/post/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   get '/about' => 'home#about'
   get 'post/index' => 'post#index'
+  get 'post/new' => 'post#new'
+  post 'post/create' => 'post#create'
+  get 'post/:id' => 'post#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
