152677eb7d3587aa570b04ab237912f04aa5311e
kanglerian <kanglerian@gmail.com>
2021-09-16 19:50:57 +0700

    Ruby on Rails5 VI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/create' => 'users#create'
+  post "users/:id/update" => "users#update"
+  get 'users/index' => 'users#index'
+  get 'signup' => 'users#new'
+  get 'users/:id' => 'users#show'
   get '/' => 'home#top'
   get '/about' => 'home#about'
   get 'post/index' => 'post#index'
